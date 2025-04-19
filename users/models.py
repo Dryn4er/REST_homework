b0e0b41c76abfcb9f2ba77f81b4519108b2f090e
Andrey <mr.dryn4er@gmail.com>
2025-04-19 14:02:30 +0300

    default

--- users/models.py
+++ users/models.py
@@ -4,4 +4,38 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        unique=True, verbose_name="почта", help_text="Введите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите аватар",
+    )
+    town = models.CharField(
+        max_length=35,
+        verbose_name="город",
+        blank=True,
+        null=True,
+        help_text="Введите название города",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
