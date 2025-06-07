e0e8a4c050dc7c210f28ea68e93a9816159d6fa5
Andrey <mr.dryn4er@gmail.com>
2025-06-07 16:36:35 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -216,14 +216,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 if 'test' in sys.argv:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("POSTGRES_DB"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("POSTGRES_HOST"),
-            "PORT": os.getenv("POSTGRES_PORT", default="5432"),
-            "OPTIONS": {"client_encoding": "utf-8"},
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'default_db_name'),
+            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         }
     }
-
